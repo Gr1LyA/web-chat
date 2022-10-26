e0d2e602eb95450ab6616077427c89e170aeab8d
Gr1LyA <milkivey1999@mail.ru>
2022-10-26 16:45:55 +0300

    add nats reconnect

--- backend/goservice/main.go
+++ backend/goservice/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/nats-io/nats.go"
@@ -14,34 +16,44 @@ const natsURL = "nats://nats:4222"
 var upgrader = websocket.Upgrader{}
 
 func main() {
-	http.HandleFunc("/ws", ws)
+	nc, err := nats.Connect(natsURL, nats.ReconnectWait(time.Second), nats.MaxReconnects(600))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer nc.Close()
+
+	http.HandleFunc("/ws", middleware(nc))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func ws(w http.ResponseWriter, r *http.Request) {
-	nc, err := nats.Connect(natsURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	connection, _ := upgrader.Upgrade(w, r, nil)
-	defer connection.Close()
-	defer nc.Close()
-
-	// Получаем message из nats и отправляем по вебсокету а фронт
-	nc.Subscribe("nats-server", func(msg *nats.Msg) {
-		connection.WriteMessage(websocket.TextMessage, msg.Data)
-	})
-
-	// Читаем из сокета и отправляем в nats
-	for {
-		mt, message, err := connection.ReadMessage()
-
-		if err != nil || mt == websocket.CloseMessage {
-			break // Выходим из цикла, если клиент пытается закрыть соединение или связь прервана
+func middleware(nc *nats.Conn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		connection, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
 		}
-		nc.Publish("nats-server", message)
-		fmt.Println("[message]", string(message))
+		defer connection.Close()
+
+		log.Println("client connected to ws")
+
+		// Получаем message из nats и отправляем по вебсокету а фронт
+		nSub, err := nc.Subscribe("nats-server", func(msg *nats.Msg) {
+			connection.WriteMessage(websocket.TextMessage, msg.Data)
+		})
+		defer nSub.Unsubscribe()
+
+		// Читаем из сокета и отправляем в nats
+		for {
+			mt, message, err := connection.ReadMessage()
+			if err != nil || mt == websocket.CloseMessage {
+				break // Выходим из цикла, если клиент пытается закрыть соединение или связь прервана
+			}
+
+			nc.Publish("nats-server", message)
+			log.Println("[message]", string(message))
+		}
 	}
 }
